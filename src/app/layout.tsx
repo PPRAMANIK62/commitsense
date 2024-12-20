5332ededecc71f95b08e849079f167bd85c6ff1b
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-12-21 00:29:30 +0530

    ui: changed font to JetBrains Mono

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import { GeistSans } from "geist/font/sans";
+import {JetBrains_Mono} from "next/font/google"
 import { type Metadata } from "next";
 
 import { TRPCReactProvider } from "@/trpc/react";
@@ -13,12 +14,16 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+const JetBrainsMono = JetBrains_Mono({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+})
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <ClerkProvider>
-      <html lang="en" className={`${GeistSans.variable}`}>
+      <html lang="en" className={`${JetBrainsMono.className}`}>
         <body>
           <TRPCReactProvider>{children}</TRPCReactProvider>
           <Toaster richColors />
